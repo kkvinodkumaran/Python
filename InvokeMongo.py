f9ecd470e3b12ed6eacefe72831f5173c5e06aaa
kkvinodkumaran <kkvinod.kumaran@gmail.com>
2020-06-07 02:34:01 -0700

    Create InvokeMongo.py

---
+++ InvokeMongo.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 00:41:15 2020
+
+@author: vinodkariyathungalkumaran
+"""
+import mypythonmongo
+
+mypythonmongo.MyMongo.saveToMongo()
